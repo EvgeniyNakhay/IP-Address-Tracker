274df897169cf088b00e0fe33ba5347fa41e56af
EvgeniyNakhay <qwert57894@gmail.com>
2025-05-25 16:05:46 +0300

    created handling user input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log("Hi");
+const ipInput = document.querySelector(".search-bar__input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", getData);
+ipInput.addEventListener("keydown", handleKey);
+
+function getData() {
+  // проверка данных
+
+  fetch(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_grC6CY2oBjqQiOzIVspZ8tIb4XJUN&ipAddress=${ipInput.value}`
+  )
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === "Enter") {
+    getData();
+  }
+}
+
+// 10.111.201.22
