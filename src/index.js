db8af6f65ee1434eb377cd5992d1680409d2b368
EvgeniyNakhay <qwert57894@gmail.com>
2025-05-25 19:12:28 +0300

    added validation of entered IP

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { validateIp } from "./helpers";
+
 const ipInput = document.querySelector(".search-bar__input");
 const btn = document.querySelector("button");
 
@@ -5,13 +7,13 @@ btn.addEventListener("click", getData);
 ipInput.addEventListener("keydown", handleKey);
 
 function getData() {
-  // проверка данных
-
-  fetch(
-    `https://geo.ipify.org/api/v2/country,city?apiKey=at_grC6CY2oBjqQiOzIVspZ8tIb4XJUN&ipAddress=${ipInput.value}`
-  )
-    .then((response) => response.json())
-    .then(console.log);
+  if (validateIp(ipInput.value)) {
+    fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_grC6CY2oBjqQiOzIVspZ8tIb4XJUN&ipAddress=${ipInput.value}`
+    )
+      .then((response) => response.json())
+      .then(console.log);
+  }
 }
 
 function handleKey(e) {
